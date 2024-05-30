653eafd936d6a2b3cdf081326022d68462827d3f
Vinicio Mendes <contatoviniciom@gmail.com>
2024-05-30 02:42:15 -0300

    Arrumei o erro de pacote no route.ts do Cleck WebHook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,8 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { Webhook } from 'svix'
  
 export async function POST(req: Request) {
  
