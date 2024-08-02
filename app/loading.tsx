808ba852442e2300abf88c5d0d248ff785e2571c
ImGabreuw <imgabreuw.dev@gmail.com>
2024-08-02 16:29:01 -0300

    feat: add fallback pages

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { LucideLoaderCircle } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-background">
+      <LucideLoaderCircle className="size-12 animate-spin" />
+      <p className="text-lg font-medium text-foreground">Carregando...</p>
+    </div>
+  );
+}
