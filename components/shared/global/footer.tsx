7a80b8f40e13275f579f33d714f92a381c931e18
ImGabreuw <imgabreuw.dev@gmail.com>
2024-08-02 17:56:33 -0300

    fix: footer responsiveness

--- components/shared/global/footer.tsx
+++ components/shared/global/footer.tsx
@@ -7,21 +7,31 @@ import { SocialMedias } from "./social-medias";
 
 import { Muted } from "@/components/typography/muted";
 import Nguzu from "@/public/assets/images/nguzu.svg";
+import { HTMLAttributes } from "react";
+
+interface NguzuLogoProps extends HTMLAttributes<HTMLElement> {}
+
+function NguzuLogo({ className, ...rest }: NguzuLogoProps) {
+  return (
+    <Link
+      href={
+        "https://www.linkedin.com/pulse/nguzu-project-como-criamos-e-concretizamos-tiago-cardoso/"
+      }
+      className={`space-y-2 ${className}`}
+      {...rest}
+    >
+      <Muted>Powered By</Muted>
+      <Image src={Nguzu} alt={"Nguzu"} width={180} height={60} />
+    </Link>
+  );
+}
 
 export const Footer: React.FC = () => {
   return (
     <footer className="bg-background text-foreground py-6">
       <div className="container mx-auto flex flex-col items-center space-y-4">
-        <div className="w-full flex justify-around items-center">
-          <Link
-            href={
-              "https://www.linkedin.com/pulse/nguzu-project-como-criamos-e-concretizamos-tiago-cardoso/"
-            }
-            className="space-y-2"
-          >
-            <Muted>Powered By</Muted>
-            <Image src={Nguzu} alt={"Nguzu"} width={180} height={60} />
-          </Link>
+      <div className="w-full flex justify-around items-center">
+          <NguzuLogo className="hidden md:block"/>
           <Link href={"https://firethebox.com/about"}>
             <Image
               src={Logo}
@@ -32,11 +42,16 @@ export const Footer: React.FC = () => {
             />
           </Link>
         </div>
+
+
         <P className="text-center">
           Uma startup, spin-off do fundo de impacto Nguzu, que busca
           democratizar o empreendedorismo e inovação, através de ambientes
           propícios para potencializar Pessoas, Ideias e Projetos.
         </P>
+
+        <NguzuLogo className="md:hidden" />
+
         <P className="text-center text-muted-foreground">
           © firethebox.com. Todos os direitos reservados.
         </P>
