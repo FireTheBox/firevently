c6f344224f3320fe52da098920dedaaec2aaa610
ImGabreuw <imgabreuw.dev@gmail.com>
2024-08-03 12:59:40 -0300

    fix: add redirect when sign in with Google

--- components/shared/auth/provider-sign-in-form.tsx
+++ components/shared/auth/provider-sign-in-form.tsx
@@ -8,7 +8,7 @@ export function ProviderSignInForm() {
     <form
       action={async () => {
         "use server";
-        await signIn("google");
+        await signIn("google", { redirect: true, redirectTo: "/" });
       }}
       className="mx-auto w-fit"
     >
